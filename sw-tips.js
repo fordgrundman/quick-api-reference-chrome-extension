fd06847f6043a38f3479f241e381fe5f11205321
Ford <codingasford@gmail.com>
2025-08-23 20:47:06 -0700

    Add content script to fetch and display tips from service worker

--- sw-tips.js
+++ sw-tips.js
@@ -26,3 +26,11 @@ createAlarm();
 
 //update tip once a day
 chrome.alarms.onAlarm.addListener(updateTip);
+
+//send tip to content script via messaging
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting === "tip") {
+    chrome.storage.local.get("tip").then(sendResponse);
+    return true;
+  }
+});
